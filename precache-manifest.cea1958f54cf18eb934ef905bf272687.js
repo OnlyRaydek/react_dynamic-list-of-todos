4259546c5f603d699a8104c9efc8fc5edb0e2eba
OnlyRaydek <bayramsaid00@gmail.com>
2020-11-08 23:49:51 +0200

    Updates

---
+++ precache-manifest.cea1958f54cf18eb934ef905bf272687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83b3612067ab5e3d59ce0dcb8977c20",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2b471169504ab70cae7",
+    "url": "./static/css/main.eabbf225.chunk.css"
+  },
+  {
+    "revision": "2be444cabb932727fd12",
+    "url": "./static/js/2.52b26eb6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.52b26eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b471169504ab70cae7",
+    "url": "./static/js/main.1d234b5b.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
